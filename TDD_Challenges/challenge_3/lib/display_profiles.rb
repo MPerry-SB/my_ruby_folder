ea1af8ddc24bfcaff1fd7dde25defd4252290df2
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2022-10-13 19:58:25 +0100

    GREEN REFACTOR: can return employee id filtered by age

--- TDD_Challenges/challenge_3/lib/display_profiles.rb
+++ TDD_Challenges/challenge_3/lib/display_profiles.rb
@@ -1,5 +1,8 @@
 class DisplayProfiles
-  def my_method(hr_profile, age:)
-    111
+  def filter_employee_by_age(hr_profile, age:)
+    hr_profile.select do |_key, hash|
+      hash[:age] == age
+      return hash[:employee_id]
+    end
   end
 end
--- TDD_Challenges/challenge_3/spec/display_profiles_spec.rb
+++ TDD_Challenges/challenge_3/spec/display_profiles_spec.rb
@@ -1,18 +1,19 @@
 require './lib/display_profiles'
 
 RSpec.describe DisplayProfiles do
-  it 'filters employee by age' do
+  it 'returns one employee id filtered by age' do
     hr_profile = {
       pwu: { employee_id: 111, age: 65 },
       jwatson: { employee_id: 456, age: 27 }
     }
     described_class = DisplayProfiles.new
 
-    result = described_class.my_method(hr_profile, age: 65)
+    result = described_class.filter_employee_by_age(hr_profile, age: 65)
 
     expect(result).to eq 111
   end
 
+  example 'returns two employee ids filterefd by age'
   example 'is employee working on a given date'
   example 'sort employees by location'
 end
