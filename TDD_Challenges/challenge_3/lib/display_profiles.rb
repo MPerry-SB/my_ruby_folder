60be802c4c802c892dceea38a88ddc3e7d0bd5a9
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2022-10-13 20:09:13 +0100

    GREEN REFACTOR: check age is bigger than or equal to given age as per requirement

--- TDD_Challenges/challenge_3/lib/display_profiles.rb
+++ TDD_Challenges/challenge_3/lib/display_profiles.rb
@@ -2,10 +2,11 @@ class DisplayProfiles
   def filter_employee_by_age(hr_profile, age:)
     results = []
     hr_profile.select do |_key, hash|
-      if hash[:age] == age
+      if hash[:age] >= age
         results << hash[:employee_id]
       end
     end
     results
   end
 end
+a
